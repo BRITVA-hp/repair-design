7966085d9c556f4d7f67e6dfc1e7cff4563b0240
BRITVA-hp <borbritvahp@gmail.com>
2020-05-06 21:21:54 +0400

    feat: add gulp-sass in gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,34 @@
-const gulp = require('gulp');
+const {src, dest, watch} = require('gulp');
 const browserSync = require('browser-sync').create();
+const rename = require('gulp-rename');
+const cssmin = require('gulp-cssmin');
+const sass = require('gulp-sass')
 // Static server
-gulp.task('browser-sync', function() {
+function bs() {
+    serveSass();
   browserSync.init({
       server: {
           baseDir: "./"
       }
   });
-  gulp.watch("./*.html").on('change', browserSync.reload);
-});
+  watch("./*.html").on('change', browserSync.reload);
+  watch("./sass/**/*.sass", serveSass);
+  watch("./js/*.js").on('change', browserSync.reload);
+};
 
 
-var cssmin = require('gulp-cssmin');
-var rename = require('gulp-rename');
- 
-gulp.task('mincss', function () {
-    gulp.src('css/*.css')
+function serveSass() {
+    return src("./sass/*.sass")
+        .pipe(sass())
+        .pipe(dest("./css"))
+        .pipe(browserSync.stream());
+};
+
+exports.serve = bs;
+
+function mincss() {
+    src('css/*.css')
         .pipe(cssmin())
         .pipe(rename({suffix: '.min'}))
         .pipe(gulp.dest('mincss'));
-});
+};
