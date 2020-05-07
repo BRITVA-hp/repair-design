fdc605e1764fc6f2a916689c01af3b5d7cbcf3af
BRITVA-hp <borbritvahp@gmail.com>
2020-05-07 19:15:11 +0400

    feat: add Futura Book & Demi fonts

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,8 @@ const browserSync = require('browser-sync').create();
 const rename = require('gulp-rename');
 const cssmin = require('gulp-cssmin');
 const sass = require('gulp-sass')
+const autoprefixer = require('gulp-autoprefixer');
+
 // Static server
 function bs() {
     serveSass();
@@ -18,8 +20,11 @@ function bs() {
 
 
 function serveSass() {
-    return src("./sass/*.sass")
+    return src("./sass/**/*.sass")
         .pipe(sass())
+        .pipe(autoprefixer({
+            cascade: false
+        }))
         .pipe(dest("./css"))
         .pipe(browserSync.stream());
 };
