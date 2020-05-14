f9af8b4abec884f8085e94cb3956220d1bc4b6d4
BRITVA-hp <borbritvahp@gmail.com>
2020-05-15 00:52:31 +0400

    refact: rewrote js code to jQuery syntax

--- js/main.js
+++ js/main.js
@@ -1,14 +1,37 @@
-document.addEventListener("DOMContentLoaded", function(event) { 
-  const modal = document.querySelector('.modal');
-  const modalBtn = document.querySelectorAll('[data-toggle=modal]');
-  const closeBtn = document.querySelector('.modal__close')
-  const switchModal = () => {
-    modal.classList.toggle('modal--visible')
-  }
-  modalBtn.forEach(element => {
-    element.addEventListener('click', switchModal);
+// document.addEventListener("DOMContentLoaded", function(event) { 
+//   const modal = document.querySelector('.modal');
+//   const modalBtn = document.querySelectorAll('[data-toggle=modal]');
+//   const closeBtn = document.querySelector('.modal__close')
+//   const switchModal = () => {
+//     modal.classList.toggle('modal--visible')
+//   };
+  
+//   modalBtn.forEach(element => {
+//     element.addEventListener('click', switchModal);
+//   });
+
+//   closeBtn.addEventListener('click', switchModal);
+//   document.addEventListener('keypress', (event) => {
+//         if (event.keyCode === 27) {
+//           modal.classList.remove('modal--visible');
+//         }
+//       });
+
+//   modal.addEventListener('click', (event) => {
+//     if (event.target == modal) {
+//       modal.classList.remove('modal--visible');
+//     }
+//   });
+// });
+$(document).ready(function () {
+  const modal = $('.modal'),
+        modalBtn = $('[data-toggle=modal]'),
+        closeBtn = $('.modal__close');
+
+  modalBtn.on('click', function (){
+    modal.toggleClass('modal--visible');
   });
-
-  closeBtn.addEventListener('click', switchModal);
-
+  closeBtn.on('click', function () {
+    modal.toggleClass('modal--visible');
+  });
 });
